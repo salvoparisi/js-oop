03fbc5b7e9d25307ca1010beaef0682c350a579d
salvoparisi <psalvo065@gmail.com>
2024-10-22 15:26:02 +0200

    add bonus

--- main.js
+++ main.js
@@ -24,7 +24,26 @@ class Veicolo {
     }
 }
 
+class Automobile extends Veicolo {
+    gomme
+    ccMotore
+    topSpeed
+
+    constructor(marca, anno, colore, numPorte, carburante, gomme, ccMotore, topSpeed) {
+        super(marca, anno, colore, numPorte, carburante)
+        this.gomme = gomme
+        this.ccMotore = ccMotore
+        this.topSpeed = topSpeed
+    }
+
+    porteAndGas() {
+        console.log(`La macchina ha ${this.numPorte} porte ed è alimentata a ${this.carburante}`);
+    }
+}
+
 const myCar = new Veicolo("Fiat", 2019, "Blu")
 myCar.informazioni()
 myCar.calcoloEta()
 
+const mySecondCar = new Automobile("Opel", 2020, "nera", 4, "benzina")
+mySecondCar.porteAndGas()
